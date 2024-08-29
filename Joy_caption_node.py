ac65b5b471a1ca03f5fa1ac1462cf50eddfb927d
q5sys (JT) <jt@obs-sec.com>
2024-08-28 21:16:17 -0400

    Update Joy_caption_node.py

--- Joy_caption_node.py
+++ Joy_caption_node.py
@@ -1,3 +1,4 @@
+
 from huggingface_hub import InferenceClient
 from torch import nn
 from transformers import AutoModel, AutoProcessor, AutoTokenizer, PreTrainedTokenizer, PreTrainedTokenizerFast, AutoModelForCausalLM
@@ -14,32 +15,32 @@ from .lib.xmodel import *
 class JoyPipeline:
     def __init__(self):
         self.clip_model = None
-        self.clip_processor = None
+        self.clip_processor =None
         self.tokenizer = None
         self.text_model = None
         self.image_adapter = None
         self.parent = None
-
+    
     def clearCache(self):
         self.clip_model = None
-        self.clip_processor = None
+        self.clip_processor =None
         self.tokenizer = None
         self.text_model = None
-        self.image_adapter = None
+        self.image_adapter = None 
 
 
 class ImageAdapter(nn.Module):
-    def __init__(self, input_features: int, output_features: int):
-        super().__init__()
-        self.linear1 = nn.Linear(input_features, output_features)
-        self.activation = nn.GELU()
-        self.linear2 = nn.Linear(output_features, output_features)
-
-    def forward(self, vision_outputs: torch.Tensor):
-        x = self.linear1(vision_outputs)
-        x = self.activation(x)
-        x = self.linear2(x)
-        return x
+	def __init__(self, input_features: int, output_features: int):
+		super().__init__()
+		self.linear1 = nn.Linear(input_features, output_features)
+		self.activation = nn.GELU()
+		self.linear2 = nn.Linear(output_features, output_features)
+	
+	def forward(self, vision_outputs: torch.Tensor):
+		x = self.linear1(vision_outputs)
+		x = self.activation(x)
+		x = self.linear2(x)
+		return x
 
 class Joy_caption_load:
 
@@ -53,8 +54,8 @@ class Joy_caption_load:
     def INPUT_TYPES(s):
         return {
             "required": {
-                "model": (["unsloth/Meta-Llama-3.1-8B-bnb-4bit", "meta-llama/Meta-Llama-3.1-8B"],),
-
+                "model": (["unsloth/Meta-Llama-3.1-8B-bnb-4bit", "meta-llama/Meta-Llama-3.1-8B"],), 
+               
             }
         }
 
@@ -63,39 +64,40 @@ class Joy_caption_load:
     FUNCTION = "gen"
 
     def loadCheckPoint(self):
+        # 清除一波
         if self.pipeline != None:
-            self.pipeline.clearCache()
-
-        # clip
+            self.pipeline.clearCache() 
+       
+         # clip
         model_id = "google/siglip-so400m-patch14-384"
         CLIP_PATH = download_hg_model(model_id,"clip")
 
-        clip_processor = AutoProcessor.from_pretrained(CLIP_PATH)
+        clip_processor = AutoProcessor.from_pretrained(CLIP_PATH) 
         clip_model = AutoModel.from_pretrained(
                 CLIP_PATH,
                 trust_remote_code=True
             )
-
+            
         clip_model = clip_model.vision_model
         clip_model.eval()
         clip_model.requires_grad_(False)
         clip_model.to("cuda")
 
-
+       
         # LLM
         MODEL_PATH = download_hg_model(self.model,"LLM")
-        tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH, use_fast=False)
+        tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH,use_fast=False)
         assert isinstance(tokenizer, PreTrainedTokenizer) or isinstance(tokenizer, PreTrainedTokenizerFast), f"Tokenizer is of type {type(tokenizer)}"
 
-        text_model = AutoModelForCausalLM.from_pretrained(MODEL_PATH, device_map="auto", trust_remote_code=True)
+        text_model = AutoModelForCausalLM.from_pretrained(MODEL_PATH, device_map="auto",trust_remote_code=True)
         text_model.eval()
 
         # Image Adapter
-        adapter_path = os.path.join(folder_paths.models_dir,"Joy_caption","image_adapter.pt")
+        adapter_path =  os.path.join(folder_paths.models_dir,"Joy_caption","image_adapter.pt")
 
-        image_adapter = ImageAdapter(clip_model.config.hidden_size, text_model.config.hidden_size)
+        image_adapter = ImageAdapter(clip_model.config.hidden_size, text_model.config.hidden_size) # ImageAdapter(clip_model.config.hidden_size, 4096) 
         image_adapter.load_state_dict(torch.load(adapter_path, map_location="cpu"))
-        adjusted_adapter = image_adapter
+        adjusted_adapter =  image_adapter #AdjustedImageAdapter(image_adapter, text_model.config.hidden_size)
         adjusted_adapter.eval()
         adjusted_adapter.to("cuda")
 
@@ -104,12 +106,12 @@ class Joy_caption_load:
         self.pipeline.tokenizer = tokenizer
         self.pipeline.text_model = text_model
         self.pipeline.image_adapter = adjusted_adapter
-
+    
     def clearCache(self):
          if self.pipeline != None:
               self.pipeline.clearCache()
 
-    def gen(self, model):
+    def gen(self,model):
         if self.model == None or self.model != model or self.pipeline == None:
             self.model = model
             self.loadCheckPoint()
@@ -126,7 +128,7 @@ class Joy_caption:
             "required": {
                 "joy_pipeline": ("JoyPipeline",),
                 "image": ("IMAGE",),
-                "prompt": ("STRING", {"multiline": True, "default": "A descriptive caption for this image"},),
+                "prompt":   ("STRING", {"multiline": True, "default": "A descriptive caption for this image"},),
                 "max_new_tokens":("INT", {"default": 300, "min": 10, "max": 1000, "step": 1}),
                 "temperature": ("FLOAT", {"default": 0.5, "min": 0.0, "max": 1.0, "step": 0.01}),
                 "cache": ("BOOLEAN", {"default": False}),
@@ -136,17 +138,18 @@ class Joy_caption:
     CATEGORY = "CXH/LLM"
     RETURN_TYPES = ("STRING",)
     FUNCTION = "gen"
+    def gen(self,joy_pipeline,image,prompt,max_new_tokens,temperature,cache): 
 
-    def gen(self, joy_pipeline, image, prompt, max_new_tokens, temperature, cache):
-
-        if joy_pipeline.clip_processor == None:
-            joy_pipeline.parent.loadCheckPoint()
+        if joy_pipeline.clip_processor == None :
+            joy_pipeline.parent.loadCheckPoint()    
 
         clip_processor = joy_pipeline.clip_processor
         tokenizer = joy_pipeline.tokenizer
         clip_model = joy_pipeline.clip_model
         image_adapter = joy_pipeline.image_adapter
         text_model = joy_pipeline.text_model
+
+     
 
         input_image = tensor2pil(image)
 
@@ -156,7 +159,6 @@ class Joy_caption:
 
         # Tokenize the prompt
         prompt = tokenizer.encode(prompt, return_tensors='pt', padding=False, truncation=False, add_special_tokens=False)
-
         # Embed image
         with torch.amp.autocast_mode.autocast('cuda', enabled=True):
             vision_outputs = clip_model(pixel_values=pImge, output_hidden_states=True)
@@ -212,6 +214,6 @@ class Joy_caption:
         r = caption.strip()
 
         if cache == False:
-            joy_pipeline.parent.clearCache()
+           joy_pipeline.parent.clearCache()  
 
         return (r,)
