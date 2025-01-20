8de765f442fdd5c636e4dad45830458c72bb6887
DICTION\chenxinghua <455758525@qq.com>
2025-01-20 17:46:55 +0800

    joy 多批量文件夹打标

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from .Joy_caption_node import Joy_caption
 from .miniCPMv2_6_prompt_generator import CXH_HG_Model_Load,CXH_Min2_6_prompt_Run
 from .florence_nodes import CXH_DownloadAndLoadFlorence2Model,CXH_Florence2Run
 from .miniCpMV3_4_chat import  CXH_MinCP3_4B_Load,CXH_MinCP3_4B_Chat
-from .Joy_caption_alpha import Joy_caption_alpha_load,Joy_caption_alpha_run,Joy_caption_alpha_prompt,Joy_caption_alpha_batch
+from .Joy_caption_alpha import Joy_caption_alpha_load,Joy_caption_alpha_run,Joy_caption_alpha_prompt,Joy_caption_alpha_batch,Joy_caption_alpha_batch_Dirs
 from .CXH_Min2_6_classifiy import CXH_Min2_6_classifiy
 from .ic_lora_batch import CXH_IC_Lora_Florence2Run,CXH_Ic_lora_Joy_batch,CXH_IC_lora_reversal
 from .smolvlm import CXH_SmolVlm_Load,CXH_SmolVlm_Run
@@ -28,6 +28,7 @@ NODE_CLASS_MAPPINGS = {
     "CXH_IC_lora_reversal":CXH_IC_lora_reversal,
     "CXH_SmolVlm_Load":CXH_SmolVlm_Load,
     "CXH_SmolVlm_Run":CXH_SmolVlm_Run,
+    "Joy_caption_alpha_batch_Dirs":Joy_caption_alpha_batch_Dirs
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -49,5 +50,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "CXH_IC_lora_reversal":"CXH_IC_lora_reversal",
     "CXH_SmolVlm_Load":"CXH_SmolVlm_Load",
     "CXH_SmolVlm_Run":"CXH_SmolVlm_Run",
+    "Joy_caption_alpha_batch_Dirs":"Joy_caption_alpha_batch_Dirs"
 }
 
